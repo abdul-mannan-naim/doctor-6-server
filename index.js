c4bf4aaa42f4f7d5b2fbc667bc392d9f1c58a979
abdul-mannan-naim <fahadmannan0001@gmail.com>
2023-02-11 00:17:19 +0600

    Allah vorosh

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+
+const app = express()
+
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgwma2r.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+const authHeader =req.headers.authorization;
+if(!authHeader){
+    return res.status(401).send("unauthorized access")
+}
+const token =authHeader.split(" ")[1];
+
+jwt.verify(token, process.env.ACCESS_TOKEN, function(err,decoded){ 
+    if(err){
+        return res.status(403).send({message:"forbidden access"})
+    }
+    req.decoded=decoded;
+    next()
+})
+
+
+}
+
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctorsPortal').collection("appointOptions")
+        const bookingsCollection = client.db('doctorsPortal').collection("bookings")
+        const usersCollection = client.db('doctorsPortal').collection("users")
+
+        app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const query = {}
+            const options = await appointmentOptionCollection.find(query).toArray()
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray()
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+                const bookedSlots = optionBooked.map(book => book.slot)
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                option.slots = remainingSlots
+            })
+            res.send(options)
+        })
+
+        app.get('/bookings',  async (req, res) => {
+            const email = req.query.email;
+            // const decodedEamil =req.decoded.email;
+            // if(email !==decodedEamil){
+            //     return res.status(403).send({message:"forbidden access"})
+            // }
+            const query = { email: email }
+            const bookings = await bookingsCollection.find(query).toArray()
+            res.send(bookings)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                treatment: booking.treatment,
+                email: booking.email,
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray()
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
+            const result = await bookingsCollection.insertOne(booking)
+            res.send(result)
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: "1h" })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: "" })
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', async (req, res) => {
+    res.send("doctor portal server is running")
+
+})
+
+app.listen(port, () => {
+    console.log(`Doctor Portal running on ${port}`)
+})
+
+
+
+
+
